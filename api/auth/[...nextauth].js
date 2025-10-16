7bb0d91bb034a3b67c7fd2ef99bc59198830c435
Chairi Balatzia <xarismpalatzias@gmail.com>
2025-10-16 22:22:26 +0100

    Fix NextAuth crash

--- api/auth/[...nextauth].js
+++ api/auth/[...nextauth].js
@@ -1,16 +1,16 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 
-export default NextAuth({
+export const authOptions = {
   providers: [
     CredentialsProvider({
       name: "Shopify",
       credentials: {
-        password: { label: "Password", type: "password" }
+        password: { label: "Password", type: "password" },
       },
-      authorize: async (credentials) => {
+      async authorize(credentials) {
         if (credentials.password === process.env.ADMIN_PASSWORD) {
-          return { id: 1, name: "CJB Admin" };
+          return { id: "1", name: "CJB Admin" };
         } else {
           throw new Error("Invalid password");
         }
@@ -18,4 +18,6 @@ export default NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
-});
+};
+
+export default NextAuth(authOptions);
